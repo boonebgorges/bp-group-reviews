06add966512b5fc05733cc934c5d79b9997b2a62
Boone Gorges <boonebgorges@gmail.com>
2011-08-01 14:07:05 -0400

    Don't try to add groups_template data on single group pages

--- bp-group-reviews.php
+++ bp-group-reviews.php
@@ -174,8 +174,11 @@ class BP_Group_Reviews {
 	function groups_template_data( $has_groups ) {
 		global $groups_template, $wpdb, $bp;
 		
+		if ( !bp_is_directory() )
+			return $has_groups;
+		
 		$group_ids = array();
-		foreach( $groups_template->groups as $group ) {
+		foreach( (array)$groups_template->groups as $group ) {
 			$group_ids[] = $group->id;
 		}
 		$group_ids = implode( ',', $group_ids );
