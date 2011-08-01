069554f7119896c65e3879c9163dcc2cb899da40
Boone Gorges <boonebgorges@gmail.com>
2011-07-31 21:30:04 -0400

    Whitespace cleanup

--- includes/schema.php
+++ includes/schema.php
@@ -42,31 +42,31 @@ class BP_Group_Reviews_Schema {
 	function register_post_type() {
 		// Define the labels to be used by the post type		
 		$post_type_labels = array(
-			'name' => _x( 'BP Reviews', 'post type general name', 'bpgr' ),
-			'singular_name' => _x( 'Review', 'post type singular name', 'bpgr' ),
-			'add_new' => _x( 'Add New', 'add new', 'bpgr' ),
-			'add_new_item' => __( 'Add New Review', 'bpgr' ),
-			'edit_item' => __( 'Edit Review', 'bpgr' ),
-			'new_item' => __( 'New Review', 'bpgr' ),
-			'view_item' => __( 'View Review', 'bpgr' ),
-			'search_items' => __( 'Search Reviews', 'bpgr' ),
-			'not_found' =>  __( 'No Reviews found', 'bpgr' ),
-			'not_found_in_trash' => __( 'No Reviews found in Trash', 'bpgr' ),
-			'parent_item_colon' => ''
+			'name' 			=> _x( 'BP Reviews', 'post type general name', 'bpgr' ),
+			'singular_name' 	=> _x( 'Review', 'post type singular name', 'bpgr' ),
+			'add_new' 		=> _x( 'Add New', 'add new', 'bpgr' ),
+			'add_new_item' 		=> __( 'Add New Review', 'bpgr' ),
+			'edit_item' 		=> __( 'Edit Review', 'bpgr' ),
+			'new_item' 		=> __( 'New Review', 'bpgr' ),
+			'view_item' 		=> __( 'View Review', 'bpgr' ),
+			'search_items' 		=> __( 'Search Reviews', 'bpgr' ),
+			'not_found' 		=>  __( 'No Reviews found', 'bpgr' ),
+			'not_found_in_trash' 	=> __( 'No Reviews found in Trash', 'bpgr' ),
+			'parent_item_colon' 	=> ''
 		);
 	
 		// Set up the arguments to be used when the post type is registered
 		// Only filter this if you are hella smart and/or know what you're doing
 		$bpgr_post_type_args = apply_filters( 'bpgr_post_type_args', array(
-			'label' => __( 'BP Reviews', 'bpgr' ),
-			'labels' => $post_type_labels,
-			'public' => false,
-			'_builtin' => false,
-			'show_ui' => $this->show_cpt_ui(),
-			'hierarchical' => false,
-			'supports' => array( 'title', 'editor', 'revisions', 'excerpt' ),
-			'query_var' => true,
-			'rewrite' => false // Todo: This bites
+			'label' 	=> __( 'BP Reviews', 'bpgr' ),
+			'labels' 	=> $post_type_labels,
+			'public' 	=> false,
+			'_builtin' 	=> false,
+			'show_ui' 	=> $this->show_cpt_ui(),
+			'hierarchical' 	=> false,
+			'supports' 	=> array( 'title', 'editor', 'revisions', 'excerpt' ),
+			'query_var'	=> true,
+			'rewrite' 	=> false // Todo: This bites
 		) );
 	
 		// Register the post type
@@ -75,32 +75,32 @@ class BP_Group_Reviews_Schema {
 		// Now, set up the user and group taxonomies
 		// Define the labels
 		$user_labels = array(
-			'name' => __( 'Reviewed Users', 'bpgr' ),
+			'name' 		=> __( 'Reviewed Users', 'bpgr' ),
 			'singular_name' => __( 'Reviewed User', 'bpgr' )
 		);
 		
 		// Register the user taxonomy
 		register_taxonomy( $this->user_tax_name, array( $this->post_type_name ), array(
-			'labels' => $user_labels,
-			'hierarchical' => false,
-			'show_ui' => true, 
-			'query_var' => true,
-			'rewrite' => array( 'slug' => 'reviewed_user' ),
+			'labels' 	=> $user_labels,
+			'hierarchical' 	=> false,
+			'show_ui' 	=> true, 
+			'query_var' 	=> true,
+			'rewrite' 	=> array( 'slug' => 'reviewed_user' ),
 		));
 		
 		// Define the labels
 		$group_labels = array(
-			'name' => __( 'Reviewed Groups', 'bpgr' ),
+			'name' 		=> __( 'Reviewed Groups', 'bpgr' ),
 			'singular_name' => __( 'Reviewed Group', 'bpgr' )
 		);
 		
 		// Register the user taxonomy
 		register_taxonomy( $this->group_tax_name, array( $this->post_type_name ), array(
-			'labels' => $group_labels,
-			'hierarchical' => false,
-			'show_ui' => true, 
-			'query_var' => true,
-			'rewrite' => array( 'slug' => 'reviewed_user' ),
+			'labels' 	=> $group_labels,
+			'hierarchical' 	=> false,
+			'show_ui' 	=> true, 
+			'query_var' 	=> true,
+			'rewrite' 	=> array( 'slug' => 'reviewed_user' ),
 		));
 	}
 	
